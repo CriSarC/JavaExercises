55ec1704ca87d0d7361570c1e4777ca6fa1ef106
Cristian Sarmiento <sarmientocastrocris@gmail.com>
2021-08-01 16:31:41 -0500

    Create ejercicio2.java para resolver el segundo ejercicio
    Realiza un conversor de pesos a dolar. La cantidad en pesos
    que se quiere convertir deberá estar almacenada en una variable.
    El valor de un dolar es: $3.895 pesos

---
+++ EjerciciosJava/1. Variables/1.EjerciciosVariables/src/pkg1/ejerciciosvariables/Ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+Realiza un conversor de pesos a dolar. La cantidad en pesos
+que se quiere convertir deberá estar almacenada en una variable.
+El valor de un dolar es: $3.895 pesos
+ */
+package pkg1.ejerciciosvariables;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.print("ingrese dinero en pesos colombianos a conveertir: ");
+        Scanner scanner = new Scanner(System.in);
+        double dineroPesos = scanner.nextInt();
+        double tasaConversion = 3895;
+  
+        
+        System.out.printf("la conversion de: " + dineroPesos  + " es: " + "%.2f%n",(dineroPesos/tasaConversion) );
+    }
+    
+}
