6dbaad2c80fe13b20f6a464ea5cb8ed12134cc36
SamuelJimenezJ <salejiji@gmail.com>
2021-08-01 15:26:32 -0500

    Saludo a los programadores
    Ejercicio 1

--- EjerciciosJava/1. Variables/1.EjerciciosVariables/src/pkg1/ejerciciosvariables/EjerciciosVariables.java
+++ EjerciciosJava/1. Variables/1.EjerciciosVariables/src/pkg1/ejerciciosvariables/EjerciciosVariables.java
@@ -16,6 +16,9 @@ public class EjerciciosVariables {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola Mi Guauuu");
+        
     }
     
 }
