8d159cc2b6bab09ab26334a5d42bfab5b64cf144
Cristian Sarmiento <sarmientocastrocris@gmail.com>
2021-08-01 16:59:59 -0500

    Create Ejercicio3Variables.java
    Escribe un programa que calcule el total de una factura a
    partir de la base de la compra (precio sin IVA). La base de
    la compra estará almacenada en una variable.
    
    Salida en consola:
    
    Base sin IVA:  $3500
    IVA: $665
    Total: $4165

---
+++ EjerciciosJava/1. Variables/1.EjerciciosVariables/src/pkg1/ejerciciosvariables/Ejercicio3Variables.java
@@ -0,0 +1,28 @@
+/*
+Escribe un programa que calcule el total de una factura a
+partir de la base de la compra (precio sin IVA). La base de
+la compra estará almacenada en una variable.
+
+Salida en consola:
+
+Base sin IVA:  $3500
+IVA: $665
+Total: $4165
+
+ */
+package pkg1.ejerciciosvariables;
+
+import java.util.Scanner;
+
+public class Ejercicio3Variables {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el precio total de la compra: ");
+        double costoBase = scanner.nextFloat();
+        int iva = 665;
+        System.out.printf("Base sin IVA: " + "%.2f%n",(costoBase-iva));
+        System.out.println("IVA: " + iva);
+        System.out.println("Total: " + costoBase);
+    }
+}
