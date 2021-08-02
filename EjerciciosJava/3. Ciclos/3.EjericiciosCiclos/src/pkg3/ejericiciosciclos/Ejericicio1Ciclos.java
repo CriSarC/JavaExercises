170861bfe34cd711f44a074ffd564240d189cfb7
Cristian Sarmiento <sarmientocastrocris@gmail.com>
2021-08-02 11:18:35 -0500

    Create Ejericicio1Ciclos.java
    Muestra los números múltiplos de 8 de 0 a 100 utilizando un bucle while.

---
+++ EjerciciosJava/3. Ciclos/3.EjericiciosCiclos/src/pkg3/ejericiciosciclos/Ejericicio1Ciclos.java
@@ -0,0 +1,22 @@
+
+package pkg3.ejericiciosciclos;
+
+/**
+ *Muestra los números múltiplos de 8 de 
+ *0 a 100 utilizando un bucle while.
+ * @author crist
+ */
+public class Ejericicio1Ciclos {
+
+    public static void main(String[] args) {
+       int contador = 1;
+        do{
+           if(contador%8==0){
+               System.out.println(contador);
+               contador++;
+           }
+           contador++;
+       }while(contador < 101);
+    }
+    
+}
