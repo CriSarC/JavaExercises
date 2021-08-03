b9c2ecbffe29b0fcbb1a3abd7806712f23791e11
Cristian Sarmiento <sarmientocastrocris@gmail.com>
2021-08-03 15:29:00 -0500

    Resolucion Reto1Variante1
    se soluciono en su totaliad la variante, ademas a eso se generó un codigo de prueba

--- Retos/Reto 1/Reto1Variante1/src/reto1variante1/Reto1Variante1.java
+++ Retos/Reto 1/Reto1Variante1/src/reto1variante1/Reto1Variante1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package reto1variante1;
 
 /**
@@ -11,11 +7,103 @@ package reto1variante1;
  */
 public class Reto1Variante1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /**
+         * Creación del personaje usando la clase Personaje
+         */
+        Personaje explorer = new Personaje("Explorador",'m');
+        /**
+         * se mueve el personaje 
+         */
+        explorer.moverDerecha(2);
+        /**
+         * muestra del estado actual del Personaje por consola
+         */
+        System.out.println("nombre: "+ explorer.getNombre());
+        System.out.println("sexo: "+ explorer.getSexo());
+        System.out.println("posicionX: "+ explorer.getPosicionX());
+        System.out.println("PosicionY: "+ explorer.getPosicionY());
+        System.out.println("distanciaTotal: "+ explorer.getDistanciaTotal());
+        System.out.println("numeroBotiquines: "+ explorer.getNumeroBotiquines());
+        System.out.println("vida: "+ explorer.getVida());
+          /**
+         * se mueve el personaje 
+         */
+        explorer.moverAbajo(5);
+        /**
+         * muestra del estado actual del Personaje por consola
+         */
+        System.out.println("nombre: "+ explorer.getNombre());
+        System.out.println("sexo: "+ explorer.getSexo());
+        System.out.println("posicionX: "+ explorer.getPosicionX());
+        System.out.println("PosicionY: "+ explorer.getPosicionY());
+        System.out.println("distanciaTotal: "+ explorer.getDistanciaTotal());
+        System.out.println("numeroBotiquines: "+ explorer.getNumeroBotiquines());
+        System.out.println("vida: "+ explorer.getVida());
+          /**
+         * se mueve el personaje 
+         */
+        explorer.moverIzquierda(1);
+        /**
+         * muestra del estado actual del Personaje por consola
+         */
+        System.out.println("nombre: "+ explorer.getNombre());
+        System.out.println("sexo: "+ explorer.getSexo());
+        System.out.println("posicionX: "+ explorer.getPosicionX());
+        System.out.println("PosicionY: "+ explorer.getPosicionY());
+        System.out.println("distanciaTotal: "+ explorer.getDistanciaTotal());
+        System.out.println("numeroBotiquines: "+ explorer.getNumeroBotiquines());
+        System.out.println("vida: "+ explorer.getVida());
+        /**
+         * mostrar la distancia total con respecto al origen de coordenadas
+         */
+        System.out.println(explorer.calcularDistanciaRespectoOrigen());
+        /**
+         * se le infringe daño al personaje
+         */
+        explorer.setVida(explorer.getVida()-40);
+        System.out.println(explorer.getVida());
+        /**
+         * muestra del estado actual del Personaje por consola
+         */
+        System.out.println("nombre: "+ explorer.getNombre());
+        System.out.println("sexo: "+ explorer.getSexo());
+        System.out.println("posicionX: "+ explorer.getPosicionX());
+        System.out.println("PosicionY: "+ explorer.getPosicionY());
+        System.out.println("distanciaTotal: "+ explorer.getDistanciaTotal());
+        System.out.println("numeroBotiquines: "+ explorer.getNumeroBotiquines());
+        System.out.println("vida: "+ explorer.getVida());
+        /**
+         * se recojen 3 botiquines
+         */
+        explorer.recojerBotiquin();
+        explorer.recojerBotiquin();
+        explorer.recojerBotiquin();
+        /**
+         * muestra del estado actual del Personaje por consola
+         */
+        System.out.println("nombre: "+ explorer.getNombre());
+        System.out.println("sexo: "+ explorer.getSexo());
+        System.out.println("posicionX: "+ explorer.getPosicionX());
+        System.out.println("PosicionY: "+ explorer.getPosicionY());
+        System.out.println("distanciaTotal: "+ explorer.getDistanciaTotal());
+        System.out.println("numeroBotiquines: "+ explorer.getNumeroBotiquines());
+        System.out.println("vida: "+ explorer.getVida());
+        /**
+         * se usan dos botiquines
+         */
+        explorer.usarBotiquin();
+        explorer.usarBotiquin();
+         /**
+         * muestra del estado actual del Personaje por consola
+         */
+        System.out.println("nombre: "+ explorer.getNombre());
+        System.out.println("sexo: "+ explorer.getSexo());
+        System.out.println("posicionX: "+ explorer.getPosicionX());
+        System.out.println("PosicionY: "+ explorer.getPosicionY());
+        System.out.println("distanciaTotal: "+ explorer.getDistanciaTotal());
+        System.out.println("numeroBotiquines: "+ explorer.getNumeroBotiquines());
+        System.out.println("vida: "+ explorer.getVida());
     }
     
 }
