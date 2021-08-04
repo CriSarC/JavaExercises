cf649eff1c07f182da5c9419b18770dc4984a053
Cristian Sarmiento <sarmientocastrocris@gmail.com>
2021-08-03 20:59:05 -0500

    Solucion Reto1Variante2
    se soluciono en su totaliad la variante, ademas a eso se generó un codigo de prueba

--- Retos/Reto 1/Reto1Variante2/src/reto1variante2/Reto1Variante2.java
+++ Retos/Reto 1/Reto1Variante2/src/reto1variante2/Reto1Variante2.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package reto1variante2;
 
 /**
@@ -15,7 +11,135 @@ public class Reto1Variante2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /**
+         * se instancia el objeto cliente
+         */
+        SIMCard cliente = new SIMCard("3015308969");
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * se recargan 50 000 pesos
+         */
+        cliente.recargarSaldo(50000);
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * se compran 12 GB de datos
+         */
+        cliente.comprarDatos(12);
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * el calificador intenta usar 3GB de plan de datos
+         */
+        cliente.consumirDatos(3);
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * se enciende el celular y se hace una llamada de 125 segundos
+         */
+        cliente.gestionarEncendidoCelular();
+        cliente.llamar(120);
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * se enciende el modo avión y enciende los datos
+         */
+        cliente.gestionarModoAvion();
+        cliente.gestionarDatos();
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * apaga el modo avion y enciende datos moviles
+         */
+        cliente.gestionarModoAvion();
+        cliente.gestionarDatos();
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * consume 3 GB
+         */
+        cliente.consumirDatos(3);
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
+        /**
+         * se apaga el celular
+         */
+        cliente.gestionarEncendidoCelular();
+        /**
+         * se muestran los datos del cliente
+         */
+        System.out.println("empresaTelefonica: " +cliente.getEmpresaTelefonia());
+        System.out.println("saldo: "+cliente.getSaldo());
+        System.out.println("numeroTelefonico: "+cliente.getNumeroTelefono());
+        System.out.println("celularApagado: "+cliente.isCelularApagado());
+        System.out.println("modoAvionActivado: "+cliente.isModoAvionActivado());
+        System.out.println("datosActicados: "+cliente.isDatosActivados());
+        System.out.println("saldoDatos: "+cliente.getSaldoDatos());
     }
     
 }
