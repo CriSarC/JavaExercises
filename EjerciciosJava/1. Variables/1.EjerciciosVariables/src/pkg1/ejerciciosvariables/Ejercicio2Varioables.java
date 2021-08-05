0c9dbb7d0a1f2851cd4e3649641177806faa72cb
S4muelJimenez <salejiji@gmail.com>
2021-08-05 08:21:16 -0500

    Ejercicio 1 solucion
    Agrege mas funcionalidad para que se puedan ver las dos soluciones

--- EjerciciosJava/1. Variables/1.EjerciciosVariables/src/pkg1/ejerciciosvariables/Ejercicio2Varioables.java
+++ EjerciciosJava/1. Variables/1.EjerciciosVariables/src/pkg1/ejerciciosvariables/Ejercicio2Varioables.java
@@ -10,13 +10,34 @@ import java.util.Scanner;
 public class Ejercicio2Varioables {
     public static void main(String[] args) {
         
-        System.out.print("ingrese dinero en pesos colombianos a convertir: ");
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Escoja la solución: ");
+        System.out.println("1 - Chalo Gonzalo ");
+        System.out.println("2 - Samuel J");
+        int solucionUsuario = scanner.nextInt();
+        
+        switch (solucionUsuario){
+           
+        case 1: // Caso de Chalo Gonzalo
+        System.out.print("ingrese dinero en pesos colombianos a convertir: ");
         double dineroPesos = scanner.nextDouble();
         double tasaConversion = 3895;
         System.out.printf("la conversion a dolares de: " + dineroPesos  + " es: " + "%.2f%n",(dineroPesos/tasaConversion) );
+        break;
         
+        case 2: // Caso de Samuel J
+        System.out.print("ingrese dinero en pesos colombianos a convertir: ");
+        float dineroIngresado = scanner.nextFloat();
+        float tasaConver = 3895;
+        System.out.printf("la conversion a dolares de: " + dineroIngresado  + " es: " + "%.5f%n",(dineroIngresado/tasaConver) );
+        break;
         
+        default:
+        System.out.printf("Opción no valida, ejecute nuevamente el programa" );    
+        
+       }
+  
     }
     
 }
