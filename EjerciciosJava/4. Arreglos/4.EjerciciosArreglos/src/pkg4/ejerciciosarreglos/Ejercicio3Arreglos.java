388f2ae81db009ca549c3ce4b5bf56c822d8b18c
S4muelJimenez <salejiji@gmail.com>
2021-08-06 17:41:04 -0500

    Ejercicios de arrays solucionados

--- EjerciciosJava/4. Arreglos/4.EjerciciosArreglos/src/pkg4/ejerciciosarreglos/Ejercicio3Arreglos.java
+++ EjerciciosJava/4. Arreglos/4.EjerciciosArreglos/src/pkg4/ejerciciosarreglos/Ejercicio3Arreglos.java
@@ -13,11 +13,20 @@ import java.util.Scanner;
 public class Ejercicio3Arreglos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Escoja la solución: ");
+        System.out.println("1 - Chalo Gonzalo ");
+        System.out.println("2 - Samuel J");
+        int solucionUsuario = scanner.nextInt();
+        
         int [] numeros = new int[8];
         for (int i=0;i<8;i++){
             System.out.print("Ingrese un número entero: ");
             numeros[i] = scanner.nextInt();
         }
+        switch (solucionUsuario){
+           
+        case 1: // Caso de Chalo Gonzalo
+        
         for(int j=0;j<8;j++){
             if(numeros[j]%2==0){
                 System.out.println(numeros[j]+" par");
@@ -25,6 +34,17 @@ public class Ejercicio3Arreglos {
             else{
                 System.out.println(numeros[j]+" impar");
             }
-        }
+        }   
+        break;
+        case 2: // Caso de Samuel J
+        for(int i=0;i<8;i++){
+          String signo= (numeros[i]%2==0) ? "par":"impar";
+          System.out.println(numeros[i]+" "+signo);
+      }    
+        break;
+        default:
+        System.out.printf("Opción no valida, ejecute nuevamente el programa" );    
+
     }
+}
 }
